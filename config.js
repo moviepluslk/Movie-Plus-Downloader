fb0d04e593d02621adb61f039686521d6030db2b
moviepluslk <mahagedara.site@gmail.com>
2025-04-02 19:22:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=en5ghaoI#JulgRHTKoCPf4r9nG2SDXB8G9Bu6FPs1LAMtV5lkQ1s' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3uYhkbZD#WVLC4bDIfqJweeZFnHVXz3OvoMu4Uk8GajbWYI4A4Lg' : process.env.SESSION_ID,
 };
